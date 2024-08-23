3755ded46938455de4b9425857dfded85693a499
progmatix21 <80444847+progmatix21@users.noreply.github.com>
2024-08-23 20:02:44 +0530

    Create talk_briefly client API, fixed indents and get_options endpoint.

--- talk_briefly.py
+++ talk_briefly.py
@@ -1,37 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+# Briefly:talk_briefly.py - client library module for Briefly REST summarizer service
+# Copyright (C) 2024  Atrij Talgery: github.com/progmatix21
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# https://www.gnu.org/licenses/agpl.txt
+# https://spdx.org/licenses/AGPL-3.0-or-later.html
+"""
+
 import requests
-import json
 import abc
-import sys
-
-'''
-print(requests.get("http://127.0.0.1:8000/").json())
-print(requests.get("http://127.0.0.1:8000/").json()['message'])
-
-print("Current options are: ")
-print(requests.get("http://127.0.0.1:8000/options").json())
-
-new_opt = {'filename': "", 'merge_threshold': 0.9, 'min_word_count': 2, 'passes': 1, 'summary_size': 1, 'include_context': False, 'verbose': False}
-
-print("New options are: ")
-print(json.dumps(new_opt))
-
-response = requests.put("http://localhost:8000/options",json=new_opt)
-
-print("Options have been set as: ")
-print(response.json())
-
-print("Get summary")
-with open("./Text/mayon_volcano.txt","r") as f:
-    for file_lines in f:
-        file_lines = f.readlines()
-        #print(file_lines)
 
 
-print(requests.post("http://127.0.0.1:8000/summary",
-    json={"text":f"{file_lines}"}).json()["text"])
-
-'''
-class NotImplemented(BaseException):
+class NotImplementedError(BaseException):
     # Raised when an unimplemented base class method is called
     pass
 
@@ -40,24 +20,26 @@ class ServiceUnavailableError(BaseException):
     pass
 
 class BrieflyAPI(object):
+    """Abstract base class for BrieflyAPI."""
     
     __class__ = abc.ABCMeta
 
-    
 
     def set_options(self,options):
-        raise NotImplemented
+        raise NotImplementedError
         
     def get_options(self):
-        raise NotImplemented
+        raise NotImplementedError
         
-    def get_summary(self):
-        raise NotImplemented
+    def get_summary(self,file_lines):
+        raise NotImplementedError
         
     def is_okay(self):
-        raise NotImplemented
+        raise NotImplementedError
         
-class VirtualSummarizer(BrieflyAPI):
+        
+class BrieflyClient(BrieflyAPI):
+    """Concrete class implementing BrieflyAPI interface."""
     
     top_endpoint = "/"
     options_endpoint = "/options"
@@ -65,65 +47,38 @@ class VirtualSummarizer(BrieflyAPI):
     
     def __init__(self,base_api_url):
         self.url = base_api_url
+
     
     def get_options(self):
-        '''Get the options from the REST API and return it.'''
+        """Get the options from the summarizer service via REST API and return it."""
         if self.is_okay():
-            response = requests.get(self.url+VirtualSummarizer.options_endpoint).json()
+            response = requests.get(self.url+BrieflyClient.options_endpoint).json()
             return str(response)
         else:
             return {}
+
     
     def set_options(self, options):
-    
-        #print("Old options are: ")
-        #print(json.dumps(new_opt))
-        #old_response = json.dumps(options)
-        #old_response = requests.get(self.url+VirtualSummarizer.options_endpoint).json()
-        #print(old_response)
+        """Set the options for the summarizer service REST API and return updated options."""
         
-        #options = new_opt
-        response = requests.put(self.url+VirtualSummarizer.options_endpoint,json=options)
+        response = requests.put(self.url+BrieflyClient.options_endpoint,json=options)
         
-        print("Options have been set as: ")
         return str(response.json())
 
-    def get_summary(self,file_lines):
-        print("Get summary")
-        #with open(filepath,"r") as f:
-        #    file_lines = f.read()
 
-        return(requests.post(self.url+VirtualSummarizer.summary_endpoint,
+    def get_summary(self,file_lines):
+        """Create a summary resource and get it."""
+        # Input:blob of text in file_lines
+        return(requests.post(self.url+BrieflyClient.summary_endpoint,
             json={"text":f"{file_lines}"}).json()["text"])
 
     
     def is_okay(self):
-        '''Check if connection is established.'''
-        response = requests.get(self.url+VirtualSummarizer.top_endpoint)
+        """Check if connection is established."""
+        
+        response = requests.get(self.url+BrieflyClient.top_endpoint)
         if str(response.status_code) == "200":
             return True
         else:
             raise ServiceUnavailableError
             
-'''
-if __name__ == "__main__":
-
-    url = "http://127.0.0.1:8000"
-    new_opt = {'filename': "", 'merge_threshold': 0.55, 'min_word_count': 2,
-        'passes': 2, 'summary_size': 1, 'include_context': False, 'verbose': False}
-    
-    
-    filepaths = ["./Text/mayon_volcano.txt","./Text/aeon_row5.txt","./Text/ukraine_dam.txt","./Text/hindi1.txt"]
-    mysummarizer = VirtualSummarizer(url)
-    try:
-        print(mysummarizer.set_options(new_opt))
-    except NotImplemented:
-        print("Exception: calling unimplemented base class method.")
-    
-    for filepath in filepaths:
-        try:
-            print(filepath)
-            print(mysummarizer.get_summary(filepath))
-        except NotImplemented:
-            print("Exception: calling unimplemented base class metnod.")
-'''
